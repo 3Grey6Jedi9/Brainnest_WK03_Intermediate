021b4cfb90aa0830546531132fa48969db68aeed
Darth Vader <danieltarancon@gmail.com>
2023-03-05 22:17:15 +0100

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,13 +1,2 @@
-naturals = range(25, 28123)
-not_abundants = set()
-abundants = set()
-for i in range(12, 28123):
-    if sum(j for j in range(1, i//2 + 1) if i % j == 0) > i:
-        abundants.add(i)
-        print(j)
-    if not any(i - j in abundants for j in abundants):
-        not_abundants.add(i)
-
-sum(not_abundants)
 
 
